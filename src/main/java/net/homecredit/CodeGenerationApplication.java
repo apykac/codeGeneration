b18f43ac884bdb8528ff217a7078a0bb7a6aa547
Sergey Semushev <apykac@gmail.com>
2020-05-16 02:30:44 +0300

    new project

---
+++ src/main/java/net/homecredit/CodeGenerationApplication.java
@@ -0,0 +1,7 @@
+package net.homecredit;
+
+public class CodeGenerationApplication {
+    public static void main(String[] args) {
+        //nothing
+    }
+}
---
+++ src/test/java/net/homecredit/CodeGenerationApplicationTest.java
@@ -0,0 +1,12 @@
+package net.homecredit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CodeGenerationApplicationTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
